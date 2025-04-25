263226dff31b3ae2cbcb7ca38f4780d2342f5507
Kaluzny Adrien <kaluzny@2a02-8429-80d0-fd01-4223-43ff-fe7e-9d99.rev.sfr.net>
2025-04-25 10:04:04 +0200

    depoly2

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,6 @@ export default {
     transform: {
       '^.+\\.js$': ['babel-jest', { configFile: './babel.config.js' }],
     },
-    extensionsToTreatAsEsm: ['.js'],
     testEnvironment: 'jsdom',
   };
   
